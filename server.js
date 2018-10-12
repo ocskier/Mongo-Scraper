c8bb8d5b4947b996439efdc6e4b88a7858ce2761
Pack Fan 1031 <ocskier@gmail.com>
2018-10-11 21:23:46 -0400

    latest revision

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-const MONGODB_URI =  "mongodb://localhost:27017/Mongo-Scraper";
+const MONGODB_URI =  process.env.MONGODB_URI || "mongodb://localhost:27017/Mongo-Scraper";
 console.log(MONGODB_URI);
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
