304bbbb15410b12fc68310feeff6dde87a0824bc
Pack Fan 1031 <ocskier@gmail.com>
2018-10-11 21:46:28 -0400

    latest revision

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Initialize Express
 const app = express();
